2e8ba3ecf3571ef5922315109ec90449111bb859
Dzimiela <casey.dzimiela@gmail.com>
2023-03-04 14:17:09 -0600

    reordered def statements

--- test_commands.py
+++ test_commands.py
@@ -1,20 +1,22 @@
 
+
+def test_function():
+    return 3
+    assert f() == 3
+
 def test_quit_command():
     pass
 #example addition
-def f():
-    return 3
+
 
 def test_executeA():
     return "Bookmark added!"
+    assert test_executeA() == "Bookmark added!"
 
 def test_executeB():
     return "Bookmark deleted!" 
-
-def test_function():
-    assert f() == 4
-    assert test_executeA() == "Bookmark added!"
     assert test_executeB() == "Bookmark deleted!" 
-  
 
 
+    
+    
